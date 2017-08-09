098a20bc4b69e9fc96601cc5c4b48a129a16ca86
Emmanuel Nyachoke <achachiez@gmail.com>
2017-08-09 08:42:41 +0300

    First commit with working image

---
+++ Dockerfile
@@ -0,0 +1,36 @@
+FROM tomcat:8-jre8
+
+ENV OPENMRS_HOME /root/.OpenMRS
+ENV OPENMRS_MODULES ${OPENMRS_HOME}/modules
+ENV OPENMRS_PLATFORM_URL="https://sourceforge.net/projects/openmrs/files/releases/OpenMRS_Platform_2.0.5/openmrs.war/download"
+ENV OPENMRS_REST_URL="https://modules.openmrs.org/modulus/api/releases/1616/download/webservices.rest-2.20.0.omod"
+
+ENV DB_NAME="openmrs"
+ENV OPENMRS_DB_USER="root"
+ENV OPENMRS_DB_PASS="dev"
+ENV OPENMRS_MYSQL_HOST="172.17.0.1"
+ENV OPENMRS_MYSQL_PORT="3306"
+
+# Refresh repositories and add mysql-client and libxml2-utils (for xmllint)
+# Download and Deploy OpenMRS
+# Download and copy reference application modules (if defined)
+# Unzip modules and copy to module/ref folder
+# Create database and setup openmrs db user
+RUN mkdir -p ${OPENMRS_HOME}
+RUN apt-get update && apt-get install -y mysql-client libxml2-utils \
+    && curl -L ${OPENMRS_PLATFORM_URL} -o ${CATALINA_HOME}/webapps/openmrs.war \
+    && curl -L ${OPENMRS_REST_URL} -o webservices.rest-2.20.0.omod \
+    && mkdir -p /root/temp/modules \
+    && cp webservices.rest-2.20.0.omod /root/temp/modules/
+
+# Copy OpenHMIS dependencies
+COPY modules/dependencies/2.x/*.omod /root/temp/modules/
+
+# Copy OpenMRS properties file
+COPY openmrs-runtime.properties /root/temp/
+
+EXPOSE 8080
+
+# Setup openmrs, optionally load demo data, and start tomcat
+COPY run.sh /run.sh
+ENTRYPOINT ["/run.sh"]
---
+++ run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Only load db and modules if this script is being loaded for the first time (ie, docker run)
+if [ -d "/root/temp" ]; then
+# ------------ Begin Load Database ------------
+
+echo "Using MySQL host: ${OPENMRS_MYSQL_HOST}"
+
+echo "Using MySQL port: ${OPENMRS_MYSQL_PORT}"
+
+# Ensure mysql is up
+while ! mysqladmin ping -h"$OPENMRS_MYSQL_HOST" -P $OPENMRS_MYSQL_PORT --silent; do
+    echo "Waiting for database at '$OPENMRS_MYSQL_HOST:$OPENMRS_MYSQL_PORT'..."
+    sleep 2
+done
+
+
+ # Write openmrs-runtime.properties file with linked database settings
+OPENMRS_CONNECTION_URL="connection.url=jdbc\:mysql\://$OPENMRS_MYSQL_HOST\:$OPENMRS_MYSQL_PORT/${DB_NAME}?autoReconnect\=true&sessionVariables\=default_storage_engine\=InnoDB&useUnicode\=true&characterEncoding\=UTF-8"
+echo "${OPENMRS_CONNECTION_URL}" >> /root/temp/openmrs-runtime.properties
+echo "connection.username=${OPENMRS_DB_USER}" >> /root/temp/openmrs-runtime.properties
+echo "connection.password=${OPENMRS_DB_PASS}" >> /root/temp/openmrs-runtime.properties
+
+cp /root/temp/openmrs-runtime.properties ${OPENMRS_HOME}/
+# Copy base/dependency modules to module folder
+echo "Copying module dependencies and reference application modules..."
+mkdir -pv $OPENMRS_MODULES
+cp /root/temp/modules/*.omod $OPENMRS_MODULES
+echo "Modules copied."
+
+# Cleanup temp files
+rm -r /root/temp
+fi
+
+# Set custom memory options for tomcat
+export JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Xms256m -Xmx1024m -XX:PermSize=256m -XX:MaxPermSize=512m"
+
+# Run tomcat
+catalina.sh run
